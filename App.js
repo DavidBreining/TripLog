d87e1604641c5d47f3d0f9ddeb756f942fa3e0eb
DavidBreining <davidbreining01@gmail.com>
2022-11-16 17:34:41 +0100

    neu

--- App.js
+++ App.js
@@ -1,51 +1,32 @@
-import { Text, View, StyleSheet, ScrollView, TextInput } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
-import { NavigationContainer} from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Gyroscope } from 'expo-sensors';
-
-var setData;
+import React, { useState, useEffect } from "react";
+import { Text, View, StyleSheet, ScrollView, TextInput } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Gyroscope } from "expo-sensors";
 
 const gyroscope = () => {
   Gyroscope.setUpdateInterval(100);
-  Gyroscope.addListener(setData)
-}
-
-const [{ x, y, z }, setData] = useState({
-  x: 0,
-  y: 0,
-  z: 0,
-});
+  Gyroscope.addListener(setData);
+};
 
 function UploadScreen() {
   return (
     <View>
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Home! dsfsdd</Text>
-    </View>
-    <View style={{ padding: 50 }}></View>
-    <View style={{ paddingLeft: 10, paddingRight: 10 }}>
-      <TextInput
-        style={{
-          height: 40,
-          borderColor: 'gray',
-          borderWidth: 2,
-        }}
-        defaultValue="You can type gjh me"
-      />
-    </View>
-    </View>
-  );
-}
-
-function RecordingScreen() { 
-  gyroscope;
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Gyro: {setData}</Text>
-      <Text style={styles.text}>x: {x}</Text>
-      <Text style={styles.text}>y: {y}</Text>
-      <Text style={styles.text}>z: {z}</Text>
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <Text>Home! dsfsdd</Text>
+      </View>
+      <View style={{ padding: 50 }}></View>
+      <View style={{ paddingLeft: 10, paddingRight: 10 }}>
+        <TextInput
+          style={{
+            height: 40,
+            borderColor: "gray",
+            borderWidth: 2,
+          }}
+          defaultValue="You can type gjh me"
+        />
+      </View>
     </View>
   );
 }
@@ -53,56 +34,72 @@ function RecordingScreen() {
 function InformationScreen() {
   return (
     <View>
-    <Text> dfkjds
-    </Text>
+      <Text> dfkjds kjkj</Text>
     </View>
-  )
+  );
 }
 
 const Tab = createBottomTabNavigator();
 
+export default function App() {
+  const [{ x, y, z }, setData] = useState({
+    x: 0,
+    y: 0,
+    z: 0,
+  });
 
-export default function App(){
+  function RecordingScreen() {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <Text>Gyro: {setData}</Text>
+        <Text style={styles.text}>x: {x}</Text>
+        <Text style={styles.text}>y: {y}</Text>
+        <Text style={styles.text}>z: {z}</Text>
+      </View>
+    );
+  }
+
   return (
     <NavigationContainer>
       <Tab.Navigator
         screenOptions={({ route }) => ({
           tabBarIcon: ({ focused, color, size }) => {
-            if (route.name === 'Upload') {
+            if (route.name === "Upload") {
               return (
                 <Ionicons
-                  name={ focused
-                      ? 'ios-cloud-upload'
-                      : 'ios-cloud-upload-outline'
+                  name={
+                    focused ? "ios-cloud-upload" : "ios-cloud-upload-outline"
                   }
                   size={size}
                   color={color}
                 />
               );
-            } else if (route.name === 'Recording') {
+            } else if (route.name === "Recording") {
               return (
                 <Ionicons
-                  name={focused ? 'ios-navigate' : 'ios-navigate-outline'}
+                  name={focused ? "ios-navigate" : "ios-navigate-outline"}
                   size={size}
                   color={color}
                 />
               );
-            } else if (route.name == 'Information') {
+            } else if (route.name == "Information") {
               return (
                 <Ionicons
-                  name={focused      
-                    ? 'ios-information-circle'
-                    : 'ios-information-circle-outline'}
+                  name={
+                    focused
+                      ? "ios-information-circle"
+                      : "ios-information-circle-outline"
+                  }
                   size={size}
                   color={color}
                 />
               );
             }
           },
-          tabBarInactiveTintColor: 'gray',
-          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: "gray",
+          tabBarActiveTintColor: "tomato",
         })}
-        >
+      >
         <Tab.Screen name="Upload" component={UploadScreen} />
         <Tab.Screen name="Recording" component={RecordingScreen} />
         <Tab.Screen name="Information" component={InformationScreen} />
@@ -111,3 +108,37 @@ export default function App(){
   );
 }
 
+function round(n) {
+  if (!n) {
+    return 0;
+  }
+  return Math.floor(n * 100) / 100;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    paddingHorizontal: 10,
+  },
+  text: {
+    textAlign: "center",
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    alignItems: "stretch",
+    marginTop: 15,
+  },
+  button: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#eee",
+    padding: 10,
+  },
+  middleButton: {
+    borderLeftWidth: 1,
+    borderRightWidth: 1,
+    borderColor: "#ccc",
+  },
+});
