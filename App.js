bc4404380a4f95bf72b8ba727c6438480112fae9
DavidBreining <davidbreining01@gmail.com>
2022-12-01 09:33:46 +0100

    neu

--- App.js
+++ App.js
@@ -5,6 +5,68 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Gyroscope } from "expo-sensors";
 
+import RNFetchBlob from 'react-native-fetch-blob';
+/*
+import ReactNativeForegroundService from '@supersami/rn-foreground-service';
+import RNLocation from 'react-native-location';
+
+RNLocation.configure({
+  distanceFilter: 100, // Meters
+  desiredAccuracy: {
+    ios: 'best',
+    android: 'balancedPowerAccuracy',
+  },
+  // Android only
+  androidProvider: 'auto',
+  interval: 5000, // Milliseconds
+  fastestInterval: 10000, // Milliseconds
+  maxWaitTime: 5000, // Milliseconds
+  // iOS Only
+  activityType: 'other',
+  allowsBackgroundLocationUpdates: false,
+  headingFilter: 1, // Degrees
+  headingOrientation: 'portrait',
+  pausesLocationUpdatesAutomatically: false,
+  showsBackgroundLocationIndicator: false,
+});
+let locationSubscription = null;
+let locationTimeout = null;
+
+ReactNativeForegroundService.add_task(
+  () => {
+    RNLocation.requestPermission({
+      ios: 'whenInUse',
+      android: {
+        detail: 'fine',
+      },
+    }).then((granted) => {
+      console.log('Location Permissions: ', granted);
+      // if has permissions try to obtain location with RN location
+      if (granted) {
+        locationSubscription && locationSubscription();
+        locationSubscription = RNLocation.subscribeToLocationUpdates(
+          ([locations]) => {
+            locationSubscription();
+            locationTimeout && clearTimeout(locationTimeout);
+            console.log(locations);
+          },
+        );
+      } else {
+        locationSubscription && locationSubscription();
+        locationTimeout && clearTimeout(locationTimeout);
+        console.log('no permissions to obtain location');
+      }
+    });
+  },
+  {
+    delay: 1000,
+    onLoop: true,
+    taskId: 'taskid',
+    onError: (e) => console.log('Error logging:', e),
+  },
+);
+*/
+
 const gyroscope = () => {
   Gyroscope.setUpdateInterval(100);
   Gyroscope.addListener(setData);
@@ -71,6 +133,8 @@ export default function App() {
   }, []);
 
   const { x, y, z } = data;
+  console.log(data);
+
   const RecordingScreen = () => {
     return (
       <View style={styles.container}>
